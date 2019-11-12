14042a1c5a45935a271ea8f0ede6dd9085625a2f
MaximeTroiano <maxime.troiano@gmail.com>
2019-11-12 15:36:25 +0100

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
     // Change to your "entry-point".
-    entry: "./src/index",
+    entry: ["@babel/polyfill", "./src/API"],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "app.bundle.js"
