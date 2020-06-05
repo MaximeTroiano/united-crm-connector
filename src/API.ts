3369e25d868cdd89bb58ee88ba3a209efba5c0d3
MaximeTroiano <maxime.troiano@gmail.com>
2020-06-05 13:46:35 +0200

    'Deploy'

--- src/API.ts
+++ src/API.ts
@@ -196,6 +196,7 @@ class API {
             .post("/auth/login", { username, password })
             .then(this.handleResponse)
             .then((token) => {
+                if (!token.data) return this.handleError(token);
                 if (this.debug_level == 3) this.log.result(1, "Token length", token.data.length);
                 this.token = token.data.token;
                 return token;
